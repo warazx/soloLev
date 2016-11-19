0b4dbea1793131975ed75f9aefc7eedf726a1961
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-19 05:19:59 +0100

    Added basics structure for the Order and Customer classes.

---
+++ app/src/main/java/com/example/kringlan/sololev/model/Customer.java
@@ -0,0 +1,60 @@
+package com.example.kringlan.sololev.model;
+
+public class Customer {
+    private static int idCounter = 1;
+
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String address;
+    private long createdDate;
+
+    public Customer(String name, String phoneNumber, String address) {
+        this.id = idCounter;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.createdDate = System.currentTimeMillis();
+        idCounter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(long createdDate) {
+        this.createdDate = createdDate;
+    }
+}
---
+++ app/src/main/java/com/example/kringlan/sololev/model/Order.java
@@ -0,0 +1,77 @@
+package com.example.kringlan.sololev.model;
+
+public class Order {
+    private static int idCounter = 1;
+
+    private int orderID;
+    private long orderingDate;
+    private Customer customer;
+    private boolean isDelivered;
+    private long deliveredDate;
+    private long deliveredLong;
+    private long deliveredLat;
+
+    public Order(Customer customer) {
+        this.orderID = idCounter;
+        this.orderingDate = System.currentTimeMillis();
+        this.customer = customer;
+        this.isDelivered = false;
+        idCounter++;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public long getOrderingDate() {
+        return orderingDate;
+    }
+
+    public void setOrderingDate(long orderingDate) {
+        this.orderingDate = orderingDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public boolean isDelivered() {
+        return isDelivered;
+    }
+
+    public void setDelivered(boolean delivered) {
+        isDelivered = delivered;
+    }
+
+    public long getDeliveredDate() {
+        return deliveredDate;
+    }
+
+    public void setDeliveredDate(long deliveredDate) {
+        this.deliveredDate = deliveredDate;
+    }
+
+    public long getDeliveredLong() {
+        return deliveredLong;
+    }
+
+    public void setDeliveredLong(long deliveredLong) {
+        this.deliveredLong = deliveredLong;
+    }
+
+    public long getDeliveredLat() {
+        return deliveredLat;
+    }
+
+    public void setDeliveredLat(long deliveredLat) {
+        this.deliveredLat = deliveredLat;
+    }
+}
